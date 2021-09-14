fc11536c2da6aa7a7b418900f12ccbdbea9e11f4
Sergio Alferov <80594181+Serge209@users.noreply.github.com>
2021-09-14 09:09:38 +0300

    Update cPlus_div.cs

--- div/cPlus_div/cPlus_div.cs
+++ div/cPlus_div/cPlus_div.cs
@@ -1,1 +1,28 @@
-
+#include <iostream>
+#include <exception>
+ 
+double divide(int, int);
+ 
+int main()
+{
+    int number1 = 18;
+    int number2 = 0;
+    try
+    {
+        double res = divide(number1, number2);
+        std::cout << res << std::endl;
+    }
+    catch (const std::exception& err)
+    {
+        std::cout << "Error!!!" << std::endl;
+    }
+    std::cout << "The End..." << std::endl;
+    return 0;
+}
+ 
+double divide(int number1, int number2)
+{
+    if (number2 == 0)
+        throw std::exception();
+    return number1 / number2;
+}
