f0808b3e7e524b4b7ad0f65c676b2f729d206c50
Sergio Alferov <80594181+Serge209@users.noreply.github.com>
2021-09-14 09:03:32 +0300

    Update main.py

--- div/div_python/main.py
+++ div/div_python/main.py
@@ -1,1 +1,22 @@
 
+import sys
+
+num_1 = float(input("Введите первое число: "))
+num_2 = float(input("Введите второе число: "))
+myint = 13
+myfloat = 2.80
+
+def div(num_1, num_2):
+    return num_1 / num_2
+try:
+    num_2 == 0
+except ZeroDivisionError:
+    print("Ошибка")
+else:
+    print("Ответ:",  div(num_1, num_2))
+
+
+
+
+print("В переменной Int занимает: ", sys.getsizeof(myint), "Байт")
+print("В переменной Float занимает: ", sys.getsizeof(myfloat), "Байт")
